724ff9447110e5269f3b57d06f7b0296481fedd7
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-06-27 21:32:18 +0300

    publish commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cars-app';
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required]
+  })
+
+  carsData: any;
+
+  constructor(private fb: FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.appService.getData(this.category).subscribe(carsData => this.carsData = carsData);
+  }
+
+  goScroll(target: HTMLElement, car?: any) {
+    target.scrollIntoView({behavior: 'smooth'});
+    if (car) {
+      this.priceForm.patchValue({car: car.name});
+    }
+  }
+
+  category: string = 'sport';
+  toggleCategory(category: string) {
+    this.category = category;
+    this.ngOnInit();
+  }
+
+  trans: any;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+  }
+
+  bgPos: any;
+
+  @HostListener('document:scroll', ['$event'])
+  onScroll() {
+    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+  }
+
+  onSubmit() {
+    if (this.priceForm.valid) {
+      this.appService.sendQuery(this.priceForm.value)
+        .subscribe(
+          {
+            next: (response: any) => {
+              alert(response.message);
+              this.priceForm.reset();
+            },
+            error: (response) => {
+              alert(response.error.message);
+            }
+          }
+        );
+    }
+  }
 }
